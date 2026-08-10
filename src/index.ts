454f6773238e9e4c0b03f260bcb8f811023c0049
Muhammad-Fahad-MF <imperial.king42@gmail.com>
2026-08-11 01:23:03 +0500

    Stage 1: fetch and cache HTML

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log("Hello, World!");
+import { readdirSync } from "node:fs";
+import { writeFile } from "node:fs/promises";
+
+
+const Site_URL: string = "https://books.toscrape.com";
+
+
+const RequestFromSite = async (page: string) => {
+    try{
+        const files = readdirSync("./cache");
+        if(files.includes(page)){
+            console.log("Hit!");
+            return;
+        }
+        console.log("Miss, Requesting ", page);
+        const response = await fetch(`${Site_URL}/catalogue/${page}`, {
+            method: "GET",
+            headers: {
+                "Accept": "text/html",
+                "User-Agent": "FlyRankInternship-A9/1.0 (https://github.com/Muhammad-Fahad-MF/Books-Scraper)"
+            },
+            signal: AbortSignal.timeout(30000)
+        });
+        
+        if(!response.ok){
+            throw new Error(`HTTP Error: ${response.status} ${response.statusText}`);
+        }
+        const html = await response.text();
+        await writeFile(`./cache/${page}`, html, 'utf-8')
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
+for (let i = 1; i < 4; i++){
+    let page = `page-${i}.html`;
+    RequestFromSite(page);
+} 
