38bbaa86f54abaa68449c4a71f2a0f1d0d46db4e
Muhammad Fahad <162401906+Muhammad-Fahad-MF@users.noreply.github.com>
2026-08-14 09:51:44 +0500

    Stage 2: discover three catalogue pages

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,70 @@
-import { readdirSync, readFileSync } from "node:fs";
-import { readFile, writeFile } from "node:fs/promises";
+import { readdirSync, readFileSync, writeFileSync } from "node:fs";
 import * as cheerio from "cheerio";
 
+const Site_URL: string = "https://books.toscrape.com/catalogue/";
+const URLs = new Set<URL>();
+let catalougesPages = 0, discoveredBooks = 0, uniqueBooks = 0;
 
-const Site_URL: string = "https://books.toscrape.com";
-
-
-const RequestFromSite = async (page: string) => {
-    try{
-        const files = readdirSync("./cache");
-        if(files.includes(page)){
-            console.log("Hit!");
-            return;
-        }
-        console.log("Miss, Requesting ", page);
-        const response = await fetch(`${Site_URL}/catalogue/${page}`, {
-            method: "GET",
-            headers: {
-                "Accept": "text/html",
-                "User-Agent": "FlyRankInternship-A9/1.0 (https://github.com/Muhammad-Fahad-MF/Books-Scraper)"
-            },
-            signal: AbortSignal.timeout(8000)
-        });
-        
-        if(!response.ok){
-            throw new Error(`HTTP Error: ${response.status} ${response.statusText}`);
-        }
-        const html = await response.text();
-        await writeFile(`./cache/${page}`, html, 'utf-8')
+const ReadHTML = async (url: URL): Promise<string> => {
+  try {
+    const files = readdirSync("./cache");
+    let page = url.href.split("/").pop() as string;
+    if (files.includes(page)) {
+      console.log("Hit!");
+      const html = readFileSync(`./cache/${page}`, {
+        encoding: "utf-8",
+      });
+      return html;
     }
-    catch (err) {
-        console.error(err);
-    }
-}
 
-// for (let i = 1; i < 4; i++){
-//     let page = `page-${i}.html`;
-//     RequestFromSite(page);
-// } 
-const html = readFileSync("./cache/page-1.html", {
-    encoding: 'utf-8'
-});
-console.log('here!');
-const $ = cheerio.load(html);
-let title = $('h3 a').text();
-console.log(title);
+    console.log("Miss, Requesting ", url.href);
+    setTimeout(()=>{ }, 500);
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "text/html",
+        "User-Agent":
+          "FlyRankInternship-A9/1.0 (https://github.com/Muhammad-Fahad-MF/Books-Scraper)",
+      },
+      signal: AbortSignal.timeout(8000),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP Error: ${response.status} ${response.statusText}`);
+    }
+
+    const html = await response.text();
+    writeFileSync(`./cache/${page}`, html, "utf-8");
+    return html;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+let current_page = new URL("page-1.html", Site_URL);
+
+for(let i = 1; i <= 3; i++) {
+  const html = await ReadHTML(current_page);
+  const $ = cheerio.load(html);
+  const links: string[] = $(".product_pod > h3 > a")
+    .map((_index, item) => $(item).attr("href"))
+    .get();
+  links.forEach((link) => {
+    let url = new URL(link as string, Site_URL);
+    discoveredBooks++;
+    URLs.add(url);
+  });
+  links.length = 0;
+
+  const next_href = $('.next > a').attr('href');
+  if(next_href && i <= 3) {
+      current_page = new URL(next_href, Site_URL);
+      catalougesPages++;
+  } else {
+    break;
+  }
+};
+
+uniqueBooks = URLs.size;
+console.log(`catalogue_pages: ${catalougesPages} \ndiscovered_books: ${discoveredBooks} \nunique_urls: ${uniqueBooks}`);
